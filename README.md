84eb557c7cedbdc46aea714380ca2b7c2cb83fad
arijit08 <arijitbhagavatula@gmail.com>
2023-01-26 12:18:16 +0530

    Cleaned code and added documentation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # NutriApp
  Recognise photo of food items and display nutrition information of the same
+
+This application uses Flask to develop a web app/server
+
+Get request to the server returns a basic webpage which allows the user to select an image from their local machine
+
+Doing so sends a Post request with the image, and allows app.py to classify the image based on the saved deep learning model
+
+Deep learning model itself was trained via transfer learning from imagenet model on tensorflow website, along with few more images from a training dataset
+
+Currently the model can only recognise 10 classes:
+"chicken_curry","chicken_wings","fried_rice","grilled_salmon","hamburger","ice_cream","pizza","ramen","steak","sushi"
--- app.py
+++ app.py
@@ -4,54 +4,58 @@ from flask import Flask, request, render_template
 import numpy as np
 from PIL import Image
 
+#Create Flask application - to create a server instance and pass requests to this app
 app = Flask(__name__)
 
+#Recognised food items
 class_names = ["chicken_curry","chicken_wings","fried_rice","grilled_salmon","hamburger","ice_cream","pizza","ramen","steak","sushi"]
+#Pre trained model
 model = tf.keras.models.load_model("NutriApp/nutriapp.h5", custom_objects={'KerasLayer':hub.KerasLayer})
 
-def read_image(image_path):
-  image =  tf.io.read_file(image_path) # reading image file
-  # name, exten = os.path.splitext(image_path)
-  image = tf.image.decode_image(image,channels=3)
-  image =  tf.image.resize(image, size = [224,224])# resize image
-  image = image/255 # normalise
-  return image
-
+#Function that passes image to the model and returns its output
 def predict_img(model, img, pred_type, class_names):
-  #image = read_image(filepath)
+  #Convert PIL image to np array
   img_np = tf.keras.utils.img_to_array(img)
+  
   img_tf = tf.convert_to_tensor(img_np, dtype=tf.float32)
 
-  #img_tf = tf.image.decode_image(img_tf,channels=3) #decode
+  #Resize and normalise image
   img_tf =  tf.image.resize(img_tf, size = [224,224])# resize image
   img_tf = img_tf/255 # normalise
 
+  #Actual prediction
   prediction =  model.predict(tf.expand_dims(img_tf,axis = 0))
 
   if pred_type=="binary":
     image_category = str(np.where(prediction > 0.5,class_names[1],class_names[0]))
   else:
+    #This code is multiclass (10 possible food items). Find prediction label with highest probability
     image_category = str(class_names[np.argmax(prediction)])
   return image_category
 
 @app.route('/')
 def loadpage():
+  #In case of get request on the root address, return the following html file (stored in templates folder)
   return render_template("index.htm")
 
+#Following code handles POST request to root_address/classify
 @app.route('/classify', methods = ['POST'])
 def classify():
+  #get image from HTTP POST request
   data = request.files.get("image")
   if data:
     img = Image.open(data.stream)
   else:
     return "Error, no 'image' found"
-  print("img size = " + str(img.size))
+  #Pass PIL image to get final classification
   label = predict_img(model,img, "multiclass", class_names)
   if label:
       return label
   else:
       return "Could not classify"
 
+#In deployment, run on 0.0.0.0 host and 8080 port, in debugging/local run the second line instead
+#NOTE: run on debug=False, else cd to the folder where app.py is stored before running. There seems to be a bug here
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=8080)
   #app.run(debug=False)
